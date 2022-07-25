10a69270727290f93e1e0faa5dce647d3b819f25
mgorem <oremgitonga22@gmail.com>
2022-07-25 16:30:29 +0300

    refresh javascript features

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   const Header = (props) => {
+    console.log(props);
     return (
       <>
-        <h1>{props.course}</h1>
+        <h1>{props.name}</h1>
       </>
     );
   };
@@ -18,7 +29,7 @@ const App = () => {
   const Content = (props) => {
     return (
       <>
-        <p>{props.content}</p>
+        <p>{props.parts}</p>
       </>
     );
   };
@@ -27,10 +38,7 @@ const App = () => {
     return (
       <>
         <p>
-          <p>
-            Number of exercises{" "}
-            {props.exercises1 + props.exercises2 + props.exercises3}
-          </p>
+          <p>Number of exercises {props.parts}</p>
         </p>
       </>
     );
@@ -39,14 +47,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content content={part1} />
-      <Content content={part2} />
-      <Content content={part3} />
-      <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   );
 };
